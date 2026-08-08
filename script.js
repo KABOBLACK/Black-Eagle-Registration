09b76d0c269ab9f3e12822b80bd84410ca83365e
KABOBLACK <159945531+KABOBLACK@users.noreply.github.com>
2026-08-08 15:08:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 document.querySelector("form").addEventListener("submit", function(event) {
     event.preventDefault();
 
-    const player name =
-document.queryselector('input[name="playName"]').value;
-
-    alert("Player registration submitted successfully!");
-
-});
-
-const playerNumber = Math.floor(Math.random() * 9000) + 1000;
+    const playerNumber = Math.floor(Math.random() * 9000) + 1000;
 const playerId = "BEFC-" + playerNumber;
 
 document.getElementById("playerId").textContent = playerId;
+
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const playerName =
+        document.querySelector('input[name="playerName"]').value;
+
+    alert("Welcome to Black Eagle FC, " + playerName + "!");
+});
