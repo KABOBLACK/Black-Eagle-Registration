ffed4cd8e65cbcc53ccb5cb7fcefbfb35c6a0c51
KABOBLACK <159945531+KABOBLACK@users.noreply.github.com>
2026-08-08 08:01:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 document.querySelector("form").addEventListener("submit", function(event) {
     event.preventDefault();
 
+    const player name =
+document.queryselector('input[name="playName"]').value;
+
     alert("Player registration submitted successfully!");
 
 });
+
