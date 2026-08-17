f98ddb3ccfb86c4c1ba1999b35ae6abb8c829764
KABOBLACK <159945531+KABOBLACK@users.noreply.github.com>
2026-08-17 12:47:41 +0200

    Create BEFC player object

--- script.js
+++ script.js
@@ -4,13 +4,11 @@ form.addEventListener("submit", function(event) {
     event.preventDefault();
 
     const playerName = document.getElementById("playerName").value;
-
 const dateOfBirth = document.getElementById("dateOfBirth").value;
-
-const location = document.getElementById("location").value;
-
-const position = document.getElementById("position").value;
-
+const location = 
+document.getElementById("location").value;
+const position = 
+document.getElementById("position").value;
 const preferredFoot = document.getElementById("preferredFoot").value;
 
 const player = {
