9c53bc46ce237f002eb1640b542af6e9d123885d
KABOBLACK <159945531+KABOBLACK@users.noreply.github.com>
2026-08-17 12:53:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,21 +7,21 @@ form.addEventListener("submit", function(event) {
     event.preventDefault();
 
     const playerName = document.getElementById("playerName").value;
-const dateOfBirth = document.getElementById("dateOfBirth").value;
-const location = 
+    const dateOfBirth = document.getElementById("dateOfBirth").value;
+    const location = 
 document.getElementById("location").value;
-const position = 
+    const position = 
 document.getElementById("position").value;
-const preferredFoot = document.getElementById("preferredFoot").value;
+    const preferredFoot = document.getElementById("preferredFoot").value;
 
-const player = {
-    playerName: playerName,
-    dateOfBirth: dateOfBirth,
-    location: location,
-    position: position,
-    preferredFoot: preferredFoot
-};
+    const player = {
+         playerName: playerName,
+         dateOfBirth: dateOfBirth,
+         location: location,
+         position: position,
+         preferredFoot: preferredFoot
+    };
 
-alert("Player name: " + playerName);
+    alert("Player name: " + playerName);
 
 });
